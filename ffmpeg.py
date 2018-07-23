346c81439c99dd2afc97a32ba3c504a10b5c518b
andrewgreene <andrewtheadminguy@gmail.com>
2018-07-23 11:28:45 -0400

    Update ffmpeg.py
    Initial code of ffmpeg.py

--- ffmpeg.py
+++ ffmpeg.py
@@ -1,1 +1,21 @@
+import os
+import os.path
+import datetime
+import shutil
 
+rightNow = datetime.datetime.now()
+yesterday = rightNow - datetime.timedelta(days=1)
+yesterdayString = yesterday.strftime("%y%m%d")
+path = "/path/to/directory/" + yesterdayString + "/*.jpg"
+filename = yesterdayString + ".mp4"
+output = "/path/to/output/" + filename
+
+command = "cd /c; /path/to/ffmpeg -framerate 30 -pattern_type glob -i '" + path + "' -c:v libx264 -p ix_fmt yuv420p " + output
+
+os.system(command)
+
+deleteCommand = "rm -rf /path/to/directory/" + yesterdayString
+
+os.system(deleteCommand)
+
+shutil.rmtree(path)
